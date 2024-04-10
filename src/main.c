095fb755aa1922476dea61d12f95461d406ea907
Vishnu Prakash Bharadwaj <39427770+VishnuPrakashBharadwaj@users.noreply.github.com>
2024-04-10 21:06:55 +0530

    Linearly varying delay
    Linearly varies the delay in steps of 50 ms

---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <ch32v00x.h>
+#include <debug.h>
+
+#define BLINKY_GPIO_PORT GPIOD
+#define BLINKY_GPIO_PIN GPIO_Pin_6
+#define BLINKY_CLOCK_ENABLE RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE)
+
+void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void Delay_Init(void);
+void Delay_Ms(uint32_t n);
+
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	SystemCoreClockUpdate();
+	Delay_Init();
+
+	GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+	BLINKY_CLOCK_ENABLE;
+	GPIO_InitStructure.GPIO_Pin = BLINKY_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(BLINKY_GPIO_PORT, &GPIO_InitStructure);
+
+	uint8_t ledState = 0;
+	uint8_t temp = 1;
+	uint8_t dir = 1;
+
+	while (1)
+	{
+		GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, ledState);
+		ledState ^= 1; // invert for the next run
+		Delay_Ms(temp*50);
+		temp = (dir) ? ++temp : --temp;
+		dir ^= (temp == 10 || temp == 1);
+	}
+}
+
+void NMI_Handler(void) {}
+void HardFault_Handler(void)
+{
+	while (1)
+	{
+	}
+}
